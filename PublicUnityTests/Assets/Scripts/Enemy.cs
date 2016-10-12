51552825e2af8dc76004ee0b3b779b5b8f24faaa
8beat0405 <8beat0405@gmail.com>
2016-10-12 14:14:54 +0900

    Added a scriptable object.

---
+++ PublicUnityTests/Assets/Scripts/Enemy.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	[SerializeField]
+	EnemyData data;
+
+	// Use this for initialization
+	void Awake () {
+		Debug.Log ("life is " + data.life);
+	}
+}
